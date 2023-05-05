2ec674cbb46417e8dbb79a162f2b910e6fff8bfc
KitiphopSophakhayang <115160108+KitiphopSophakhayang@users.noreply.github.com>
2023-05-05 14:18:10 +0700

    Initial commit

---
+++ src/components/vmodel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "test",
+    };
+  },
+};
+</script>
+
+
